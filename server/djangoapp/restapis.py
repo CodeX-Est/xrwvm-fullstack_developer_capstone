fd680b5f319551f731014d62386b05a46bd6b509
Brett <codex.estates@gmail.com>
2024-04-06 20:58:01 -0400

    Fixed Sentiment Analyzer

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -9,7 +9,7 @@ backend_url = os.getenv(
     'backend_url', default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    default="https://sentianalyzer.1fgpb074edvp.us-south.codeengine.appdomain.cloud/")
 
 
 def get_request(endpoint, **kwargs):
@@ -29,7 +29,7 @@ def get_request(endpoint, **kwargs):
 
 
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url+"/analyze/"+text
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
